9f6aaf4cecd7b06713318948a0f1dbee3f41ce1f
gth-softdev <68547258+gth-softdev@users.noreply.github.com>
2020-08-27 11:53:00 -0400

    Added

--- GeneralStoreAPI/Models/Product.cs
+++ GeneralStoreAPI/Models/Product.cs
@@ -7,5 +7,12 @@ namespace GeneralStoreAPI.Models
 {
     public class Product
     {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public int NumberInInventory { get; set; }
+        public bool IsInStock { get; set; }
+
+
     }
 }
---
+++ GeneralStoreAPI/Models/StoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class StoreDbContext : DbContext
+    {
+    }
+}
